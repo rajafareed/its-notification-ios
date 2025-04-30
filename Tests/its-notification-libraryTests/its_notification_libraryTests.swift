a081559b5196bc6530219ec1b078138382959cfd
rajajeee <41549157+rajajeee@users.noreply.github.com>
2025-04-30 16:34:00 +0500

    Initial Commit

---
+++ Tests/its-notification-libraryTests/its_notification_libraryTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import its_notification_library
+
+final class its_notification_libraryTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
